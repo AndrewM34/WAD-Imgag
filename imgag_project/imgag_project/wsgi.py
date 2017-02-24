5522ad09c69fd60082b5d506f2d2bdf62f8d8209
AndrewM34 <amcnab@protonmail.com>
2017-02-24 14:42:25 +0000

    created app

--- imgag_project/imgag_project/wsgi.py
+++ imgag_project/imgag_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for imgag project.
+WSGI config for imgag_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imgag.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "imgag_project.settings")
 
 application = get_wsgi_application()
