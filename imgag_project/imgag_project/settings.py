4f801b98cc989a648807a1056c030a9b8386c1f8
Petr School <2267328s@student.gla.ac.uk>
2017-03-03 16:33:35 +0000

    Setting up sending emails

--- imgag_project/imgag_project/settings.py
+++ imgag_project/imgag_project/settings.py
@@ -139,3 +139,9 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "wad.imgag@gmail.com"
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
