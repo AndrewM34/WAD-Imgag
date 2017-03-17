b631b9f427b36e06f83b9fd158c6a9aa39f526ff
AndrewM34 <amcnab@protonmail.com>
2017-03-17 16:53:30 +0000

    added some tests, they fucking suck ass though, I am not good at writing tests. Test test testy mctesterson

--- imgag_project/imgag/tests.py
+++ imgag_project/imgag/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from imgag.models import Vote, Upload, UserProfile, Comment
+from imgag.views import vote
+from populate_imgag import populate
 
-# Create your tests here.
+from datetime import datetime
+from django.utils import timezone
+from django.core.files import File
+from django.contrib.auth.models import User
+
+import os
+
+# ----------------- TESTS ----------------- #
+
+class TestVoteModel(TestCase):
+	def setUp(self):
+		populate()
+
+	# test the vote actually saves
+	def test_vote_updates(self):
+		testUser = UserProfile.objects.all().first()
+		testUpload = Upload.objects.all().first()
+		testVote = Vote(user=testUser, upload=testUpload)
+
+		testVote.vote = 1 # upboats to the left xD xD eksdee
+		testVote.save()
+		self.assertEqual(testVote.vote == 1, True)
+
+	def test_comments(self):
+		testComment = Comment.objects.all().first()
+		testComment.text = "AEEEAEAEEEAE"
+		testComment.save()
+
+		self.assertEqual(testComment.text, "AEEEAEAEEEAE")
+
+	# tests whether a comment is on the correct post
+	def test_correct_upload_comment_hashid(self):
+		testUpload = Upload.objects.all().first()
+		testComment = Comment.objects.filter(upload=testUpload).first()
+		self.assertEqual(testComment.upload, testUpload)
+
+	def test_upload_has_correct_title(self):
+		try:
+			testUpload = Upload.objects.all().first()
+			response = self.client.get(reverse('post/'+testUpload.hashid))
+		except:
+			return False
+
+		self.assertIn(testUpload.header, response.content)
