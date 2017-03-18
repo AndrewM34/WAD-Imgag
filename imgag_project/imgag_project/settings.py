26482b40fe08918b4fe1b820fb4c495554b10b3c
Petr Sramek <futurehero42@gmail.com>
2017-03-18 13:05:48 +0000

    Allowed hosts fixed...

--- imgag_project/imgag_project/settings.py
+++ imgag_project/imgag_project/settings.py
@@ -36,7 +36,7 @@ HASHID_FIELD_SALT = "as%2d3kvnq751+bucw%rc+x0b*qf*xdfty(fzmb(7a7wp6zn)l"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["imgag.pythonanywhere.com",]
+ALLOWED_HOSTS = ["imgag.pythonanywhere.com", "localhost", ]
 
 
 # Application definition
