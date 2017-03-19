b561abbb1d4ea061cddbbec6245ee8f7b92262ab
AndrewM34 <amcnab@protonmail.com>
2017-03-19 15:29:09 +0000

    quick commit as a backup point for tests

--- imgag_project/imgag/tests.py
+++ imgag_project/imgag/tests.py
@@ -2,11 +2,12 @@ from django.test import TestCase
 from imgag.models import Vote, Upload, UserProfile, Comment
 from imgag.views import vote
 from populate_imgag import populate
-
+from django.core.urlresolvers import reverse
 from datetime import datetime
 from django.utils import timezone
 from django.core.files import File
 from django.contrib.auth.models import User
+from django.contrib.auth import login
 
 import os
 
@@ -26,6 +27,7 @@ class TestVoteModel(TestCase):
 		testVote.save()
 		self.assertEqual(testVote.vote == 1, True)
 
+	# test comments save
 	def test_comments(self):
 		testComment = Comment.objects.all().first()
 		testComment.text = "AEEEAEAEEEAE"
@@ -39,11 +41,46 @@ class TestVoteModel(TestCase):
 		testComment = Comment.objects.filter(upload=testUpload).first()
 		self.assertEqual(testComment.upload, testUpload)
 
+	# tests if an upload has the correct title
 	def test_upload_has_correct_title(self):
+		testUpload = Upload.objects.all().first()
 		try:
-			testUpload = Upload.objects.all().first()
-			response = self.client.get(reverse('post/'+testUpload.hashid))
+			response = self.client.get(reverse('post', kwargs={'post_hashid':testUpload.hashid}))
 		except:
-			return False
+			print("-"*20+"Could not find an upload")
+			self.assertEqual(True, False)
 
-		self.assertIn(testUpload.header, response.content)
+		self.assertIn(bytes(testUpload.header, 'utf-8'), response.content)
+
+	# tests to see if someone can see nsfw content without logging in
+	def test_guest_can_see_nsfw(self):
+		response = self.client.get(reverse('post', kwargs={'post_hashid':'ZynyW4L'})) # a nsfw post
+
+		self.assertEqual(response.context["user_can_see_nsfw"], False)
+
+	# tests to see if a user under the age of 18 can view nsfw
+	def test_under_18_can_see_nsfw(self):
+		def add_user(username, email, date_of_birth, path_to_picture):
+			user = User.objects.get_or_create(email=email)[0]
+			user.username = username
+			user.set_password(username)
+			user.save()
+
+			my_datetime = datetime.strptime(date_of_birth, "%b %d %Y")
+			profile = \
+				UserProfile.objects.get_or_create(user=user,
+													date_of_birth=timezone.make_aware(my_datetime,
+																					timezone.get_current_timezone()))[0]
+			if path_to_picture is not None:
+				imopen = open(path_to_picture, "rb")
+				django_file = File(imopen)
+				filename = os.path.basename(path_to_picture)
+				profile.picture.save(filename, django_file, save=False)
+			profile.save()
+			return profile
+
+
+		testUser = add_user('ohmycosh', 'ohmycosh@thatdomainsucks.com', 'Jun 1 2005', None)
+
+
+		response = self.client.get(reverse('post', kwargs={'post_hashid':'ZynyW4L'}))
